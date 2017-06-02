c94c0e2124fd1e39dfaa506741615d56761baf88
Saleem Rashid <dev@saleemrashid.com>
2017-06-02 18:13:27 +0100

    CWallet: Use ISMINE_{NO,SPENDABLE} for hardware wallets

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -1400,7 +1400,17 @@ CAmount CWallet::GetDebit(const CTxIn &txin, const isminefilter& filter) const
 
 isminetype CWallet::IsMine(const CTxOut& txout) const
 {
-    return ::IsMine(*this, txout.scriptPubKey);
+    isminetype mine = ::IsMine(*this, txout.scriptPubKey);
+
+    if (IsHardwareWallet()) {
+        if (mine == ISMINE_WATCH_UNSOLVABLE) {
+            return ISMINE_NO;
+        } else if (mine == ISMINE_WATCH_SOLVABLE) {
+            return ISMINE_SPENDABLE;
+        }
+    }
+
+    return mine;
 }
 
 CAmount CWallet::GetCredit(const CTxOut& txout, const isminefilter& filter) const

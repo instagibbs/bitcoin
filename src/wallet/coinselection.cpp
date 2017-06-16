581870f97ac838bdb3d94dabd785723ab0404d6a
Andrew Chow <achow101-github@achow101.com>
2017-06-15 17:33:27 -0700

    Move original coin selector and tests
    Moved the original coin selection algorithm to coinselection.cpp and its tests
    to coin_selection_test.cpp

--- src/wallet/coinselection.cpp
+++ src/wallet/coinselection.cpp
@@ -3,6 +3,8 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include "wallet/coinselection.h"
+#include "util.h"
+#include "utilmoneystr.h"
 
 // Descending order comparator
 struct {
@@ -12,13 +14,24 @@ struct {
     }
 } descending;
 
-bool SelectCoinsBnB(std::vector<CInputCoin>& utxo_pool, const CAmount& target_value, const CAmount& cost_of_change, std::vector<CInputCoin>& out_set, FastRandomContext* rand)
+struct CompareValueOnly
 {
+    bool operator()(const CInputCoin& t1,
+                    const CInputCoin& t2) const
+    {
+        return t1.txout.nValue < t2.txout.nValue;
+    }
+};
+
+bool SelectCoinsBnB(std::vector<CInputCoin>& utxo_pool, const CAmount& target_value, const CAmount& cost_of_change, std::set<CInputCoin>& out_set, CAmount& value_ret, FastRandomContext* rand)
+{
+    out_set.clear();
+    value_ret = 0;
+
     if (utxo_pool.size() <=0) {
         return false;
     }
 
-    CAmount selected_value = 0;
     int depth = 0;
     int tries = 100000;
     std::vector<std::pair<bool, bool>> selection; // First bool: select the utxo at this index; Second bool: traversing second branch of this utxo
@@ -38,15 +51,15 @@ bool SelectCoinsBnB(std::vector<CInputCoin>& utxo_pool, const CAmount& target_va
     // Depth first search to find 
     while (!done)
     {
-        if (selected_value > target_value + cost_of_change) { // Selected value is out of range, go back and try other branch
+        if (value_ret > target_value + cost_of_change) { // Selected value is out of range, go back and try other branch
             backtrack = true;
-        } else if (selected_value >= target_value) { // Selected value is within range
+        } else if (value_ret >= target_value) { // Selected value is within range
             done = true;
         } else if (tries <= 0) { // Too many tries, exit
             done = true;
         } else if (depth >= (int)utxo_pool.size()) { // Reached a leaf node, no solution here
             backtrack = true;
-        } else if (selected_value + remaining < target_value) { // Cannot possibly reach target with amount remaining
+        } else if (value_ret + remaining < target_value) { // Cannot possibly reach target with amount remaining
             if (depth == 0) { // At the first utxo, no possible selections, so exit
                 return false;
             } else {
@@ -59,7 +72,7 @@ bool SelectCoinsBnB(std::vector<CInputCoin>& utxo_pool, const CAmount& target_va
             if (rand == nullptr || rand->randbool()) {
                 // Inclusion branch first
                 selection[depth].first = true;
-                selected_value += utxo_pool.at(depth).txout.nValue;
+                value_ret += utxo_pool.at(depth).txout.nValue;
                 ++depth;
             } else {
                 // Exclusion branch first
@@ -77,7 +90,7 @@ bool SelectCoinsBnB(std::vector<CInputCoin>& utxo_pool, const CAmount& target_va
             while (selection[depth].second) {
                 // Reset this utxo's selection
                 if (selection[depth].first) {
-                    selected_value -= utxo_pool.at(depth).txout.nValue;
+                    value_ret -= utxo_pool.at(depth).txout.nValue;
                 }
                 selection[depth].first = false;
                 selection[depth].second = false;
@@ -97,12 +110,12 @@ bool SelectCoinsBnB(std::vector<CInputCoin>& utxo_pool, const CAmount& target_va
 
                 if(selection[depth].first) { // If it was included, do exclusion now
                     selection[depth].first = false;
-                    selected_value -= utxo_pool.at(depth).txout.nValue;
+                    value_ret -= utxo_pool.at(depth).txout.nValue;
                     ++depth;
                 }
                 else { // It was excluded first, do inclusion now
                     selection[depth].first = true;
-                    selected_value += utxo_pool.at(depth).txout.nValue;
+                    value_ret += utxo_pool.at(depth).txout.nValue;
                     ++ depth;
                 }
             }
@@ -111,12 +124,147 @@ bool SelectCoinsBnB(std::vector<CInputCoin>& utxo_pool, const CAmount& target_va
     }
 
     // Set output set
-    out_set.clear();
     for (unsigned int i = 0; i < selection.size(); ++i) {
         if (selection.at(i).first) {
-            out_set.push_back(utxo_pool.at(i));
+            out_set.insert(utxo_pool.at(i));
         }
    }
+
+    return true;
+}
+
+static void ApproximateBestSubset(const std::vector<CInputCoin>& vValue, const CAmount& nTotalLower, const CAmount& nTargetValue,
+                                  std::vector<char>& vfBest, CAmount& nBest, int iterations = 1000)
+{
+    std::vector<char> vfIncluded;
+
+    vfBest.assign(vValue.size(), true);
+    nBest = nTotalLower;
+
+    FastRandomContext insecure_rand;
+
+    for (int nRep = 0; nRep < iterations && nBest != nTargetValue; nRep++)
+    {
+        vfIncluded.assign(vValue.size(), false);
+        CAmount nTotal = 0;
+        bool fReachedTarget = false;
+        for (int nPass = 0; nPass < 2 && !fReachedTarget; nPass++)
+        {
+            for (unsigned int i = 0; i < vValue.size(); i++)
+            {
+                //The solver here uses a randomized algorithm,
+                //the randomness serves no real security purpose but is just
+                //needed to prevent degenerate behavior and it is important
+                //that the rng is fast. We do not use a constant random sequence,
+                //because there may be some privacy improvement by making
+                //the selection random.
+                if (nPass == 0 ? insecure_rand.randbool() : !vfIncluded[i])
+                {
+                    nTotal += vValue[i].txout.nValue;
+                    vfIncluded[i] = true;
+                    if (nTotal >= nTargetValue)
+                    {
+                        fReachedTarget = true;
+                        if (nTotal < nBest)
+                        {
+                            nBest = nTotal;
+                            vfBest = vfIncluded;
+                        }
+                        nTotal -= vValue[i].txout.nValue;
+                        vfIncluded[i] = false;
+                    }
+                }
+            }
+        }
+    }
+}
+
+bool KnapsackSolver(std::vector<CInputCoin>& utxo_pool, const CAmount& nTargetValue, std::set<CInputCoin>& out_set, CAmount& value_ret)
+{
+    out_set.clear();
+    value_ret = 0;
+
+    // List of values less than target
+    boost::optional<CInputCoin> coinLowestLarger;
+    std::vector<CInputCoin> vValue;
+    CAmount nTotalLower = 0;
+
+    random_shuffle(utxo_pool.begin(), utxo_pool.end(), GetRandInt);
+
+    for (const CInputCoin coin : utxo_pool)
+    {
+        if (coin.txout.nValue == nTargetValue)
+        {
+            out_set.insert(coin);
+            value_ret += coin.txout.nValue;
+            return true;
+        }
+        else if (coin.txout.nValue < nTargetValue + MIN_CHANGE)
+        {
+            vValue.push_back(coin);
+            nTotalLower += coin.txout.nValue;
+        }
+        else if (!coinLowestLarger || coin.txout.nValue < coinLowestLarger->txout.nValue)
+        {
+            coinLowestLarger = coin;
+        }
+    }
+
+    if (nTotalLower == nTargetValue)
+    {
+        for (const auto& input : vValue)
+        {
+            out_set.insert(input);
+            value_ret += input.txout.nValue;
+        }
+        return true;
+    }
+
+    if (nTotalLower < nTargetValue)
+    {
+        if (!coinLowestLarger)
+            return false;
+        out_set.insert(coinLowestLarger.get());
+        value_ret += coinLowestLarger->txout.nValue;
+        return true;
+    }
+
+    // Solve subset sum by stochastic approximation
+    std::sort(vValue.begin(), vValue.end(), CompareValueOnly());
+    std::reverse(vValue.begin(), vValue.end());
+    std::vector<char> vfBest;
+    CAmount nBest;
+
+    ApproximateBestSubset(vValue, nTotalLower, nTargetValue, vfBest, nBest);
+    if (nBest != nTargetValue && nTotalLower >= nTargetValue + MIN_CHANGE)
+        ApproximateBestSubset(vValue, nTotalLower, nTargetValue + MIN_CHANGE, vfBest, nBest);
+
+    // If we have a bigger coin and (either the stochastic approximation didn't find a good solution,
+    //                                   or the next bigger coin is closer), return the bigger coin
+    if (coinLowestLarger &&
+        ((nBest != nTargetValue && nBest < nTargetValue + MIN_CHANGE) || coinLowestLarger->txout.nValue <= nBest))
+    {
+        out_set.insert(coinLowestLarger.get());
+        value_ret += coinLowestLarger->txout.nValue;
+    }
+    else {
+        for (unsigned int i = 0; i < vValue.size(); i++)
+            if (vfBest[i])
+            {
+                out_set.insert(vValue[i]);
+                value_ret += vValue[i].txout.nValue;
+            }
+
+        if (LogAcceptCategory(BCLog::SELECTCOINS)) {
+            LogPrint(BCLog::SELECTCOINS, "SelectCoins() best subset: ");
+            for (unsigned int i = 0; i < vValue.size(); i++) {
+                if (vfBest[i]) {
+                    LogPrint(BCLog::SELECTCOINS, "%s ", FormatMoney(vValue[i].txout.nValue));
+                }
+            }
+            LogPrint(BCLog::SELECTCOINS, "total %s\n", FormatMoney(nBest));
+        }
+    }
 
     return true;
 }
--- src/wallet/coinselection.h
+++ src/wallet/coinselection.h
@@ -8,9 +8,12 @@
 #include "amount.h"
 #include "primitives/transaction.h"
 #include "random.h"
-#include "wallet.h"
+#include "wallet/wallet.h"
 
 // rand can be nullptr, but only for testing. exclude_first should only ever be true in tests.
-bool SelectCoinsBnB(std::vector<CInputCoin>& utxo_pool, const CAmount& target_value, const CAmount& cost_of_change, std::vector<CInputCoin>& out_set, FastRandomContext* rand);
+bool SelectCoinsBnB(std::vector<CInputCoin>& utxo_pool, const CAmount& target_value, const CAmount& cost_of_change, std::set<CInputCoin>& out_set, CAmount& value_ret, FastRandomContext* rand);
+
+// Original coin selection algorithm as a fallback
+bool KnapsackSolver(std::vector<CInputCoin>& utxo_pool, const CAmount& nTargetValue, std::set<CInputCoin>& out_set, CAmount& value_ret);
 
 #endif // BITCOIN_COINSELECTION_H

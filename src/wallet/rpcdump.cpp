654d8b182666b16501cc43200edafa868407b0d2
Gregory Sanders <gsanders87@gmail.com>
2017-11-28 11:14:39 -0500

    Disable all key or address importing if the wallet is externalhd

--- src/wallet/rpcdump.cpp
+++ src/wallet/rpcdump.cpp
@@ -250,6 +250,9 @@ UniValue importaddress(const JSONRPCRequest& request)
             + HelpExampleRpc("importaddress", "\"myscript\", \"testing\", false")
         );
 
+    if (pwallet->IsExternalHD()) {
+        throw JSONRPCError(RPC_WALLET_ERROR, "External HD wallets are not allowed to import addresses or keys.");
+    }
 
     std::string strLabel = "";
     if (!request.params[1].isNull())
@@ -416,6 +419,9 @@ UniValue importpubkey(const JSONRPCRequest& request)
             + HelpExampleRpc("importpubkey", "\"mypubkey\", \"testing\", false")
         );
 
+    if (pwallet->IsExternalHD()) {
+        throw JSONRPCError(RPC_WALLET_ERROR, "External HD wallets are not allowed to import addresses or keys.");
+    }
 
     std::string strLabel = "";
     if (!request.params[1].isNull())
@@ -474,6 +480,10 @@ UniValue importwallet(const JSONRPCRequest& request)
             "\nImport using the json rpc call\n"
             + HelpExampleRpc("importwallet", "\"test\"")
         );
+
+    if (pwallet->IsExternalHD()) {
+        throw JSONRPCError(RPC_WALLET_ERROR, "External HD wallets are not allowed to import addresses or keys.");
+    }
 
     if (fPruneMode)
         throw JSONRPCError(RPC_WALLET_ERROR, "Importing wallets is disabled in pruned mode");
@@ -1081,6 +1091,10 @@ UniValue importmulti(const JSONRPCRequest& mainRequest)
 
             "\nResponse is an array with the same size as the input that has the execution result :\n"
             "  [{ \"success\": true } , { \"success\": false, \"error\": { \"code\": -1, \"message\": \"Internal Server Error\"} }, ... ]\n");
+
+    if (pwallet->IsExternalHD()) {
+        throw JSONRPCError(RPC_WALLET_ERROR, "External HD wallets are not allowed to import addresses or keys.");
+    }
 
     // clang-format on
 

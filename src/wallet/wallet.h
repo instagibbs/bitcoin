25b05058b9999577c8558f6c74793c6d37dcef60
Gregory Sanders <gsanders87@gmail.com>
2018-01-03 11:44:54 -0500

    dedupe external hd wallet version declaration

--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -97,7 +97,6 @@ enum WalletFeature
     FEATURE_HD = 130000, // Hierarchical key derivation after BIP32 (HD Wallet)
 
     FEATURE_HD_SPLIT = 139900, // Wallet with HD chain split (change outputs will use m/0'/1'/k)
-    FEATURE_EXTERNAL_HD = 139901, // External Hierarchical key derivation after BIP32 (HD Wallet)
 
     FEATURE_NO_DEFAULT_KEY = 159900, // Wallet without a default key written
     

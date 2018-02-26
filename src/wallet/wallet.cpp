3a2ddb5ed578cc958c0cfd7cf91bc077abb9db5c
Gregory Sanders <gsanders87@gmail.com>
2018-02-26 16:22:23 -0500

    Wallet should implicitly know segwit scripts for externalhd keys

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -181,6 +181,8 @@ CPubKey CWallet::GenerateNewKey(CWalletDB &walletdb, bool internal)
     else {
         if (!AddWatchOnly(pubkey, metadata, nCreationTime))
             throw std::runtime_error(std::string(__func__) + ": AddWatchOnly failed");
+        LOCK(cs_KeyStore);
+        ImplicitlyLearnRelatedKeyScripts(pubkey);
     }
     return pubkey;
 }

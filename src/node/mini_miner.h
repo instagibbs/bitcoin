8a7087b0e5839988fcdf93b664ac7b747baec8d6
glozow <gloriajzhao@gmail.com>
2023-07-14 11:28:00 +0100

    [MiniMiner] enable manually created MiniMinerMempoolEntrys, track sequence
    Track the order in which transactions are added to the block template,
    which is what a linearization algorithm would be interested in.

--- src/node/mini_miner.h
+++ src/node/mini_miner.h
@@ -32,6 +32,15 @@ public:
         fee_with_ancestors{entry->GetModFeesWithAncestors()},
         vsize_with_ancestors(entry->GetSizeWithAncestors())
     { }
+    explicit MiniMinerMempoolEntry(CAmount fee_individual_in, CAmount fee_with_ancestors_in,
+                                   int64_t vsize_individual_in, int64_t vsize_with_ancestors_in,
+                                   const CTransactionRef& tx_in) :
+        fee_individual{fee_individual_in},
+        tx{tx_in},
+        vsize_individual{vsize_individual_in},
+        fee_with_ancestors{fee_with_ancestors_in},
+        vsize_with_ancestors{vsize_with_ancestors_in}
+    {}
 
     CAmount GetModifiedFee() const { return fee_individual; }
     CAmount GetModFeesWithAncestors() const { return fee_with_ancestors; }
@@ -50,8 +59,11 @@ struct IteratorComparator
     }
 };
 
-/** A minimal version of BlockAssembler. Allows us to run the mining algorithm on a subset of
- * mempool transactions, ignoring consensus rules, to calculate mining scores. */
+/** A minimal version of BlockAssembler. Allows us to run the mining algorithm on a limited set of
+ * transactions (e.g. subset of mempool or a package not yet in mempool) instead of the entire
+ * mempool, ignoring consensus rules. Callers may use this to calculate mining scores, bump fees, or
+ * linearization order of a list of transactions.
+ */
 class MiniMiner
 {
     // When true, a caller may use CalculateBumpFees(). Becomes false if we failed to retrieve
@@ -67,7 +79,10 @@ class MiniMiner
     // the same tx will have the same bumpfee. Excludes non-mempool transactions.
     std::map<uint256, std::vector<COutPoint>> m_requested_outpoints_by_txid;
 
-    // What we're trying to calculate.
+    // Txid to a sequence number representing the order in which this transaction was included.
+    // Transactions included in an ancestor set together have the same sequence number.
+    std::map<uint256, uint32_t> m_mining_sequence;
+    // What we're trying to calculate. Outpoint to the fee needed to bring the transaction to the target feerate.
     std::map<COutPoint, CAmount> m_bump_fees;
 
     // The constructed block template
@@ -98,12 +113,23 @@ public:
     bool IsReadyToCalculate() const { return m_ready_to_calculate; }
 
     /** Build a block template until the target feerate is hit. */
-    void BuildMockTemplate(const CFeeRate& target_feerate);
+    void BuildMockTemplate(std::optional<CFeeRate> target_feerate);
 
     /** Returns set of txids in the block template if one has been constructed. */
     std::set<uint256> GetMockTemplateTxids() const { return m_in_block; }
 
+    /** Constructor that takes a list of outpoints that may or may not belong to transactions in
+     * mempool. */
     MiniMiner(const CTxMemPool& mempool, const std::vector<COutPoint>& outpoints);
+
+    /** Constructor in which MiniMinerMempoolEntry entries have been constructed manually,
+     * presumably because these transactions are not in the mempool (yet).
+     * @param[in] descendant_caches A map from each transaction to the set of txids of this
+     *                              transaction's descendant set, including itself. All of the
+     *                              txids must correspond to a transaction in manual_entries.
+     */
+    MiniMiner(const std::vector<MiniMinerMempoolEntry>& manual_entries,
+              const std::map<uint256, std::set<uint256>>& descendant_caches);
 
     /** Construct a new block template and, for each outpoint corresponding to a transaction that
      * did not make it into the block, calculate the cost of bumping those transactions (and their
@@ -115,6 +141,13 @@ public:
      * not make it into the block to the target feerate. Returns the total bump fee, or std::nullopt
      * if it cannot be calculated. */
     std::optional<CAmount> CalculateTotalBumpFees(const CFeeRate& target_feerate);
+
+    /** Construct a new block template with all of the transactions and calculate the order in which
+     * they are selected. Returns the sequence number (lower = selected earlier) with which each
+     * transaction was selected, indexed by txid, or an empty map if it cannot be calculated.
+     */
+    std::map<uint256, uint32_t> Linearize();
+
 };
 } // namespace node
 

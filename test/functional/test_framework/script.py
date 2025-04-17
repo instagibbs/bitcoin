ca28fa2e46104018b5eebf43e816e4e1470d4681
Greg Sanders <gsanders87@gmail.com>
2025-04-17 15:14:29 -0400

    test: Add bip348 coverage to feature_taproot

--- test/functional/test_framework/script.py
+++ test/functional/test_framework/script.py
@@ -233,7 +233,6 @@ OP_CHECKSIG = CScriptOp(0xac)
 OP_CHECKSIGVERIFY = CScriptOp(0xad)
 OP_CHECKMULTISIG = CScriptOp(0xae)
 OP_CHECKMULTISIGVERIFY = CScriptOp(0xaf)
-OP_CHECKSIGFROMSTACK = CScriptOp(0xcc)
 
 # expansion
 OP_NOP1 = CScriptOp(0xb0)
@@ -249,6 +248,9 @@ OP_NOP10 = CScriptOp(0xb9)
 
 # BIP 342 opcodes (Tapscript)
 OP_CHECKSIGADD = CScriptOp(0xba)
+
+# BIP 348 (OP_SUCCESS204)
+OP_CHECKSIGFROMSTACK = CScriptOp(0xcc)
 
 OP_INVALIDOPCODE = CScriptOp(0xff)
 

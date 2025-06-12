260bb476b455eccdc7f4f27030e2e4b6e3291529
Antoine Poinsot <darosior@protonmail.com>
2025-06-12 10:44:02 -0400

    chainparams: introduce a deployment for OP_TEMPLATEHASH
    The deployment is never active on mainnet or testnet. Activation parameters are intentionally left
    to be defined eventually and out of scope for this patchset.
    
    The deployment is always active on regtest, allowing us to extensively test expected behaviour of
    the proposed operation using the functional test suite.
    
    Co-Authored-By: Greg Sanders <gsanders87@gmail.com>

--- src/rpc/blockchain.cpp
+++ src/rpc/blockchain.cpp
@@ -1423,6 +1423,7 @@ UniValue DeploymentInfo(const CBlockIndex* blockindex, const ChainstateManager& 
     SoftForkDescPushBack(blockindex, softforks, chainman, Consensus::DEPLOYMENT_SEGWIT);
     SoftForkDescPushBack(blockindex, softforks, chainman, Consensus::DEPLOYMENT_TESTDUMMY);
     SoftForkDescPushBack(blockindex, softforks, chainman, Consensus::DEPLOYMENT_TAPROOT);
+    SoftForkDescPushBack(blockindex, softforks, chainman, Consensus::DEPLOYMENT_TEMPLATEHASH);
     return softforks;
 }
 } // anon namespace

ad874f49497aed3d85ecb61cc3bb28536fef1e5f
Greg Sanders <gsanders87@gmail.com>
2026-07-31 19:01:51 -0400

    fuzz: verify generated transaction signatures

--- src/test/fuzz/script_sign.cpp
+++ src/test/fuzz/script_sign.cpp
@@ -2,19 +2,23 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
+#include <addresstype.h>
 #include <chainparams.h>
+#include <coins.h>
 #include <key.h>
 #include <psbt.h>
 #include <pubkey.h>
 #include <script/keyorigin.h>
 #include <script/sign.h>
 #include <script/signingprovider.h>
+#include <script/solver.h>
 #include <streams.h>
 #include <test/fuzz/FuzzedDataProvider.h>
 #include <test/fuzz/fuzz.h>
 #include <test/fuzz/util.h>
 #include <test/util/transaction_utils.h>
 #include <util/chaintype.h>
+#include <util/check.h>
 #include <util/translation.h>
 
 #include <cassert>
@@ -142,4 +146,122 @@ FUZZ_TARGET(script_sign, .init = initialize_script_sign)
         SignatureData signature_data_2;
         (void)ProduceSignature(provider, DUMMY_MAXIMUM_SIGNATURE_CREATOR, ConsumeScript(fuzzed_data_provider), signature_data_2);
     }
+
+    FuzzedDataProvider valid_provider(buffer.data(), buffer.size());
+    const CKey valid_key{ConsumePrivateKey(valid_provider, /*compressed=*/true)};
+    if (!valid_key.IsValid()) return;
+
+    FillableSigningProvider valid_keys;
+    Assert(valid_keys.AddKey(valid_key));
+    const CPubKey pubkey{valid_key.GetPubKey()};
+    const CScript p2pk{GetScriptForRawPubKey(pubkey)};
+    const CScript p2pkh{GetScriptForDestination(PKHash{pubkey})};
+    const CScript multisig{GetScriptForMultisig(1, {pubkey})};
+    const size_t input_count{valid_provider.ConsumeIntegralInRange<size_t>(1, 4)};
+
+    CMutableTransaction unsigned_tx;
+    unsigned_tx.version = valid_provider.ConsumeIntegralInRange<int32_t>(1, 3);
+    unsigned_tx.nLockTime = valid_provider.ConsumeIntegral<uint32_t>();
+    std::map<COutPoint, Coin> valid_coins;
+    std::vector<bool> expect_witness;
+    for (size_t i = 0; i < input_count; ++i) {
+        const COutPoint prevout{Txid::FromUint256(ConsumeUInt256(valid_provider)), static_cast<uint32_t>(i)};
+        unsigned_tx.vin.emplace_back(prevout, CScript{}, valid_provider.ConsumeIntegral<uint32_t>());
+        unsigned_tx.vout.emplace_back(0, CScript{} << OP_TRUE);
+
+        const CScript& inner_script{valid_provider.PickValueInArray({&p2pk, &p2pkh, &multisig})[0]};
+        CScript script_pubkey;
+        bool is_witness{false};
+        switch (valid_provider.ConsumeIntegralInRange<unsigned>(0, 5)) {
+        case 0:
+            script_pubkey = inner_script;
+            break;
+        case 1:
+            Assert(valid_keys.AddCScript(inner_script));
+            script_pubkey = GetScriptForDestination(ScriptHash{inner_script});
+            break;
+        case 2:
+            Assert(valid_keys.AddCScript(inner_script));
+            script_pubkey = GetScriptForDestination(WitnessV0ScriptHash{inner_script});
+            is_witness = true;
+            break;
+        case 3: {
+            Assert(valid_keys.AddCScript(inner_script));
+            const CScript witness_program{GetScriptForDestination(WitnessV0ScriptHash{inner_script})};
+            Assert(valid_keys.AddCScript(witness_program));
+            script_pubkey = GetScriptForDestination(ScriptHash{witness_program});
+            is_witness = true;
+            break;
+        }
+        case 4:
+            script_pubkey = GetScriptForDestination(WitnessV0KeyHash{pubkey});
+            is_witness = true;
+            break;
+        case 5: {
+            const CScript witness_program{GetScriptForDestination(WitnessV0KeyHash{pubkey})};
+            Assert(valid_keys.AddCScript(witness_program));
+            script_pubkey = GetScriptForDestination(ScriptHash{witness_program});
+            is_witness = true;
+            break;
+        }
+        }
+
+        const CAmount amount{ConsumeMoney(valid_provider, /*max=*/MAX_MONEY - 1)};
+        Assert(valid_coins.emplace(prevout, Coin{CTxOut{amount, script_pubkey}, /*height=*/1, /*coinbase=*/false}).second);
+        expect_witness.push_back(is_witness);
+        Assert(IsSegWitOutput(valid_keys, script_pubkey) == is_witness);
+    }
+
+    const int sighash_type{valid_provider.PickValueInArray({
+        static_cast<int>(SIGHASH_ALL),
+        static_cast<int>(SIGHASH_NONE),
+        static_cast<int>(SIGHASH_SINGLE),
+        SIGHASH_ALL | SIGHASH_ANYONECANPAY,
+        SIGHASH_NONE | SIGHASH_ANYONECANPAY,
+        SIGHASH_SINGLE | SIGHASH_ANYONECANPAY,
+    })};
+    CMutableTransaction signed_tx{unsigned_tx};
+    std::map<int, bilingual_str> signing_errors;
+    Assert(SignTransaction(signed_tx, &valid_keys, valid_coins, {.sighash_type = sighash_type}, signing_errors));
+    Assert(signing_errors.empty());
+    Assert(signed_tx.version == unsigned_tx.version);
+    Assert(signed_tx.nLockTime == unsigned_tx.nLockTime);
+    Assert(signed_tx.vout == unsigned_tx.vout);
+    for (size_t i = 0; i < input_count; ++i) {
+        Assert(signed_tx.vin[i].prevout == unsigned_tx.vin[i].prevout);
+        Assert(signed_tx.vin[i].nSequence == unsigned_tx.vin[i].nSequence);
+    }
+
+    const CTransaction tx{signed_tx};
+    PrecomputedTransactionData txdata;
+    std::vector<CTxOut> spent_outputs;
+    for (const CTxIn& input : tx.vin) {
+        spent_outputs.push_back(valid_coins.at(input.prevout).out);
+    }
+    txdata.Init(tx, std::move(spent_outputs), /*force=*/true);
+    for (size_t i = 0; i < input_count; ++i) {
+        const Coin& coin{valid_coins.at(tx.vin[i].prevout)};
+        ScriptError error{SCRIPT_ERR_OK};
+        Assert(VerifyScript(tx.vin[i].scriptSig, coin.out.scriptPubKey, &tx.vin[i].scriptWitness, STANDARD_SCRIPT_VERIFY_FLAGS,
+                            TransactionSignatureChecker{&tx, static_cast<unsigned int>(i), coin.out.nValue, txdata, MissingDataBehavior::FAIL}, &error));
+        Assert(tx.vin[i].scriptWitness.IsNull() == !expect_witness[i]);
+
+        const SignatureData extracted{DataFromTransaction(signed_tx, i, coin.out)};
+        Assert(extracted.complete);
+        CTxIn reconstructed;
+        UpdateInput(reconstructed, extracted);
+        Assert(reconstructed.scriptSig == tx.vin[i].scriptSig);
+        Assert(reconstructed.scriptWitness.stack == tx.vin[i].scriptWitness.stack);
+    }
+
+    const CTransaction before_resign{signed_tx};
+    Assert(SignTransaction(signed_tx, &valid_keys, valid_coins, {.sighash_type = sighash_type}, signing_errors));
+    Assert(signing_errors.empty());
+    Assert(CTransaction{signed_tx} == before_resign);
+
+    FillableSigningProvider empty_provider;
+    CMutableTransaction missing_key_tx{unsigned_tx};
+    std::map<int, bilingual_str> missing_key_errors;
+    Assert(!SignTransaction(missing_key_tx, &empty_provider, valid_coins, {.sighash_type = sighash_type}, missing_key_errors));
+    Assert(missing_key_errors.size() == input_count);
 }

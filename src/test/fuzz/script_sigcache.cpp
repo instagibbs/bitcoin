3006761e52677fa97b61c0c40eb3a6bb7494e84e
Greg Sanders <gsanders87@gmail.com>
2026-07-31 18:38:36 -0400

    fuzz: verify valid signature cache entries

--- src/test/fuzz/script_sigcache.cpp
+++ src/test/fuzz/script_sigcache.cpp
@@ -3,6 +3,7 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include <consensus/amount.h>
+#include <key.h>
 #include <primitives/transaction.h>
 #include <pubkey.h>
 #include <script/interpreter.h>
@@ -13,7 +14,9 @@
 #include <test/fuzz/util.h>
 #include <test/util/setup_common.h>
 #include <uint256.h>
+#include <util/check.h>
 
+#include <array>
 #include <cstddef>
 #include <optional>
 #include <vector>
@@ -52,5 +55,67 @@ FUZZ_TARGET(script_sigcache, .init = initialize_script_sigcache)
                 (void)caching_transaction_signature_checker.VerifyECDSASignature(random_bytes, *pub_key, ConsumeUInt256(fuzzed_data_provider));
             }
         }
+    }
+
+    FuzzedDataProvider valid_provider(buffer.data(), buffer.size());
+    const CKey key{ConsumePrivateKey(valid_provider, /*compressed=*/true)};
+    if (!key.IsValid()) return;
+
+    SignatureCache valid_cache{/*max_size_bytes=*/1024};
+    SignatureCache fallback_cache{/*max_size_bytes=*/1024};
+    PrecomputedTransactionData valid_tx_data;
+    CachingTransactionSignatureChecker storing_checker{nullptr, 0, 0, /*store=*/true, valid_cache, valid_tx_data};
+    CachingTransactionSignatureChecker nonstoring_checker{nullptr, 0, 0, /*store=*/false, valid_cache, valid_tx_data};
+    CachingTransactionSignatureChecker fallback_checker{nullptr, 0, 0, /*store=*/false, fallback_cache, valid_tx_data};
+    const uint256 sighash{ConsumeUInt256(valid_provider)};
+    uint256 wrong_sighash{sighash};
+    wrong_sighash.begin()[0] ^= 1;
+
+    {
+        std::vector<unsigned char> signature;
+        Assert(key.Sign(sighash, signature));
+        const CPubKey pubkey{key.GetPubKey()};
+        uint256 entry;
+        valid_cache.ComputeEntryECDSA(entry, sighash, signature, pubkey);
+        uint256 fallback_entry;
+        fallback_cache.ComputeEntryECDSA(fallback_entry, sighash, signature, pubkey);
+
+        Assert(!fallback_cache.Get(fallback_entry, /*erase=*/false));
+        Assert(fallback_checker.VerifyECDSASignature(signature, pubkey, sighash));
+        Assert(!fallback_cache.Get(fallback_entry, /*erase=*/false));
+        Assert(storing_checker.VerifyECDSASignature(signature, pubkey, sighash));
+        Assert(valid_cache.Get(entry, /*erase=*/false));
+        Assert(!storing_checker.VerifyECDSASignature(signature, pubkey, wrong_sighash));
+        Assert(valid_cache.Get(entry, /*erase=*/false));
+        Assert(nonstoring_checker.VerifyECDSASignature(signature, pubkey, sighash));
+        Assert(valid_cache.Get(entry, /*erase=*/false));
+        Assert(nonstoring_checker.VerifyECDSASignature(signature, pubkey, sighash));
+        Assert(valid_cache.Get(entry, /*erase=*/false));
+        signature.assign(1, 0);
+        Assert(!storing_checker.VerifyECDSASignature(signature, pubkey, sighash));
+    }
+
+    {
+        std::array<unsigned char, 64> signature;
+        Assert(key.SignSchnorr(sighash, signature, /*merkle_root=*/nullptr, ConsumeUInt256(valid_provider)));
+        const XOnlyPubKey pubkey{key.GetPubKey()};
+        uint256 entry;
+        valid_cache.ComputeEntrySchnorr(entry, sighash, signature, pubkey);
+        uint256 fallback_entry;
+        fallback_cache.ComputeEntrySchnorr(fallback_entry, sighash, signature, pubkey);
+
+        Assert(!fallback_cache.Get(fallback_entry, /*erase=*/false));
+        Assert(fallback_checker.VerifySchnorrSignature(signature, pubkey, sighash));
+        Assert(!fallback_cache.Get(fallback_entry, /*erase=*/false));
+        Assert(storing_checker.VerifySchnorrSignature(signature, pubkey, sighash));
+        Assert(valid_cache.Get(entry, /*erase=*/false));
+        Assert(!storing_checker.VerifySchnorrSignature(signature, pubkey, wrong_sighash));
+        Assert(valid_cache.Get(entry, /*erase=*/false));
+        Assert(nonstoring_checker.VerifySchnorrSignature(signature, pubkey, sighash));
+        Assert(valid_cache.Get(entry, /*erase=*/false));
+        Assert(nonstoring_checker.VerifySchnorrSignature(signature, pubkey, sighash));
+        Assert(valid_cache.Get(entry, /*erase=*/false));
+        signature.fill(0xff);
+        Assert(!storing_checker.VerifySchnorrSignature(signature, pubkey, sighash));
     }
 }
